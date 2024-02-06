9cf2d20dd7790f6ac57e298ae48dcd676e199288
Mukul29-Dev <dev.mukul.3y.29@gmail.com>
2024-02-06 21:36:02 +0530

    Data-fetched

--- backend/db.js
+++ backend/db.js
@@ -4,20 +4,24 @@ const mongoose = require('mongoose');
 const mongoDB = async () => {
   try {
     // Connect to MongoDB
-    await mongoose.connect('mongodb://localhost:27017/Nutrition', {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect('mongodb://localhost:27017/Nutrition', );
 
     console.log('Connected to MongoDB');
 
-    const collection = mongoose.connection.db.collection('workout');
+    const collection = mongoose.connection.collection('workout');
 
     const data = await collection.find({}).toArray();
     console.log('Fetched data:', data);
+
+    // Access data inside each document
+    data.forEach((document) => {
+      console.log('Document:', document);
+      // Access individual fields like document.fieldName
+    });
   } catch (error) {
     console.error('Error connecting to MongoDB:', error.message);
   }
 };
+
 
 module.exports = mongoDB;
