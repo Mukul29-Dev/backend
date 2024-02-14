13715c25e616eb46dbb58f624721fbca83a962b8
Mukul29-Dev <dev.mukul.3y.29@gmail.com>
2024-02-14 18:18:25 +0530

    New-B

--- backend/db.js
+++ backend/db.js
@@ -1,4 +1,8 @@
+const express = require('express');
 const mongoose = require('mongoose');
+
+const app = express();
+const port = 5000;
 
 const mongoDB = async () => {
   try {
@@ -13,20 +17,30 @@ const mongoDB = async () => {
     console.log('Fetched data:', data);
 
     // Access data inside each document
-    data.forEach((document) => {
-      console.log('Document ID:', document._id);
-      console.log('Workout Data:');
-      document.Workout.forEach((workout) => {
-        console.log('Workout ID:', workout.id);
-        console.log('Workout Title:', workout.title);
-        console.log('Workout Image:', workout.img);
-        console.log('Workout Description:', workout.description);
-        // Access other fields as needed
-      });
-    });
+    // data.forEach((document) => {
+    //   console.log('Document ID:', document._id);
+    //   console.log('Workout Data:');
+    //   document.Workout.forEach((workout) => {
+    //     console.log('Workout ID:', workout.id);
+    //     console.log('Workout Title:', workout.title);
+    //     console.log('Workout Image:', workout.img);
+    //     console.log('Workout Description:', workout.description);
+    //     // Access other fields as needed
+    //   });
+    // });
   } catch (error) {
     console.error('Error connecting to MongoDB:', error.message);
   }
 };
 
+app.get('/workout', async (req, res) => {
+  try {
+    const users = await Workout.find({});
+    const chestWorkouts = await Workout.find({ category: 'chest' });
+    console.log('Chest Workouts:', chestWorkouts);
+  } catch (error) {
+    console.error('Error fetching users:', error.message);
+    res.status(500).send('Internal Server Error');
+  } 
+});
 module.exports = mongoDB;
--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,36 @@
-const express = require('express');
-const mongoose = require('mongoose');
+// const express = require('express');
+// const mongoose = require('mongoose');
 
-const app = express();
-const port = 5000;
-const mongoDB = require('./db');
+// const app = express();
+// const port = 5000;
+// const mongoDB = require('./db');
 
-mongoDB();
-
-// const userSchema = new mongoose.Schema();
+// mongoDB();
 
 
-// const User = mongoose.model('User', userSchema);
+// const workoutSchema = new mongoose.Schema({
+//     id: {
+//       type: Number,
+//       required: true
+//     },
+//     img: {
+//       type: String,
+//       required: true
+//     },
+//     title: {
+//       type: String,
+//       required: true
+//     },
+//     description: {
+//       type: String,
+//       required: true
+//     },
+//     category: {
+//       type: String
+//     }
+//   });
 
-
+//   const Workout = mongoose.model('Workout', workoutSchema);
 
 // app.listen(port, () => {
 //   console.log(`Server is running on port ${port}`);
@@ -21,9 +39,10 @@ mongoDB();
 // app.get('/workout', async (req, res) => {
 //   try {
 //     const users = await Workout.find({});
-//     res.json(users);
+//     const chestWorkouts = await Workout.find({ category: 'chest' });
+//     console.log('Chest Workouts:', chestWorkouts);
 //   } catch (error) {
 //     console.error('Error fetching users:', error.message);
 //     res.status(500).send('Internal Server Error');
-//   }
+//   } 
 // });
